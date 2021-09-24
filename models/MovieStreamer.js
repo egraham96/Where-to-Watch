a303da8272567e723b8c697685c7d8c0e8716a0c
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-23 20:59:50 -0700

    added movie streamer model

---
+++ models/MovieStreamer.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class MovieStreamer extends Model {}
+
+MovieStreamer.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    movie_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'movie',
+        key: 'id',
+      },
+    },
+    streamer_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'streamingservice',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'moviestreamer',
+  }
+);
+
+module.exports = MovieStreamer;
