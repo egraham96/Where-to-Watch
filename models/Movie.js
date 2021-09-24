bf5fbc908db62b6950eac3ac89c657104fe8bb43
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-23 20:50:51 -0700

    added movie model

---
+++ models/Movie.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number_of_ratings: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 0
+    },
+    rating_total: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    current_rating: {
+      type: DataTypes.INTEGER, 
+      allowNull: true,
+      defaultValue: null
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'movie',
+  }
+);
+
+module.exports = Movie;
